af163f6ce7e4bf5630a33733d521793290f8e087
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-05 15:55:09 +0300

    designed tests and all tests passing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (your name)</h1>
+      <img src="" alt="myimage"/>
+      <h2>About Me</h2>
+      <p role="biography"></p>
+      <a href="https://github.com/Ephymuiruri" role="link">Github</a>
+      <a href="https://www.linkedin.com/in/ephy-wachira-6b86532bb/" role="link">linkedin</a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,39 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test ("display a top-level heading with the text `Hi I'm _________`",()=>{
+    //Arrange
+    render(<App />);
+    // Act 
+    const topLevelHeading = screen.getByRole("heading",{
+        name:/hi, i'm/i,
+        exact:false,
+        level:1
+    })
+    //Assert
+    expect(topLevelHeading).toBeInTheDocument()
+});
+test("An image of yourself with alt text identifying the content of the image",()=>{
+  render(<App />)
+  const image =screen.getByAltText("myimage")
+  expect(image).toBeInTheDocument()
+})
+test("A second-level heading with the text About Me",()=>{
+    render (<App />)
+    const secondLevelHeading =screen.getByText("About Me",{
+        name:/About Me/i,
+        exact:false,
+        level:2
+    })
+    expect(secondLevelHeading).toBeInTheDocument()
+});
+test("A paragraph for your biography",()=>{
+    render (<App />)
+    const biography =screen.getByRole("biography")
+    expect(biography).toBeInTheDocument()
+});
+test("Two links, one to your GitHub page, and one to your LinkedIn page",()=>{
+    render(<App />)
+    const links =screen.getAllByRole("link");
+    expect(links.length).toBe(2)
+});
